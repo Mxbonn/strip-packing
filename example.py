6fe233c5db28ee2afb96337a0f6e810ffe7e048e
Maxim Bonnaerens <maxim@bonnaerens.be>
2016-12-08 11:53:11 +0100

    implemented everything but recursive_packing p4

--- example.py
+++ example.py
@@ -1,8 +1,10 @@
-from spp import visualize
+from spp import visualize, hprg
 from collections import namedtuple
 
+Rectangle = namedtuple('Rectangle', ['x', 'y', 'w', 'h'])
+
+
 def main():
-    Rectangle = namedtuple('Rectangle', ['x', 'y', 'w', 'h'])
     r1 = Rectangle(0, 0, 5, 10)
     r2 = Rectangle(5, 0, 15, 9)
     r3 = Rectangle(0, 10, 2, 14)
@@ -12,5 +14,11 @@ def main():
 
     visualize(20, 30, rectangles)
 
+def test():
+    boxes = [[5, 10], [15, 9], [2, 14], [3, 8], [10, 20]]
+    width = 10
+    height, rectangles = hprg(width, boxes)
+    visualize(width, height, rectangles)
+
 if __name__ == "__main__":
-    main()
+    test()
--- spp/core.py
+++ spp/core.py
@@ -1,22 +1,69 @@
-import sys
+from copy import deepcopy
+from collections import namedtuple
+import logging
+
+# Setup logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_logger = logging.StreamHandler()
+console_logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+console_logger.setFormatter(formatter)
+logger.addHandler(console_logger)
+
+Rectangle = namedtuple('Rectangle', ['x', 'y', 'w', 'h'])
 
 
-def sgvcp(width, height, rectangles, g_max=1000, h_max=None, n_0=None):
-    if h_max is None:
-        h_max = width
-    if n_0 is None:
-        n_0 = 100 if rectangles.size() < 500 else 200
+def hprg(width, rectangles):
+    result = [None] * len(rectangles)
+    remaining = deepcopy(rectangles)
+    logger.debug('Original itens : {}'.format(rectangles))
+    for idx, r in enumerate(remaining):
+        if r[0] > r[1]:
+            remaining[idx][0], remaining[idx][1] = remaining[idx][1], remaining[idx][0]
+    logger.debug('Items after swapping: {}'.format(remaining))
+    sorted_indices = sorted(range(len(remaining)), key=lambda x: remaining[x][0])
+    logger.debug('Indices of remaining items sorted by decreasing width: {}'.format(sorted_indices))
+    x, y, w, h, H = 0, 0, 0, 0, 0
+    while sorted_indices:
+        idx = sorted_indices.pop(0)
+        r = remaining[idx]
+        if r[1] > width:
+            result[idx] = Rectangle(x, y, r[0], r[1])
+            x, y, w, h, H = r[0], H, width - r[0], r[1], H + r[1]
+        else:
+            result[idx] = Rectangle(x, y, r[1], r[0])
+            x, y, w, h, H = r[1], H, width - r[1], r[0], H + r[0]
+        recursive_packing(x, y, w, h, remaining, sorted_indices, result)
+        x, y = 0, H
 
-    # SGVCP step 1
-    c = []
-    for w, h in rectangles:
-        c.append(w * h)
-    g = 0
-    H_best = sys.maxsize
+    return H, result
 
-    while(g < g_max):  # step 2
-        # step 3
-        g += 1
-        n_left = rectangles.size()
-        k = 0
-        # TODO: let all items be remaining ones
+
+def recursive_packing(x, y, w, h, remaining, indices, result):
+    priority = 6
+    for idx in indices:
+        for D in range(0, 2):
+            if priority > 1 and remaining[idx][(0 + D) % 2] == w and remaining[idx][(1 + D) % 2] == h:
+                priority, orientation, best = 1, D, idx
+                break
+            elif priority > 2 and remaining[idx][(0 + D) % 2] == w and remaining[idx][(1 + D) % 2] < h:
+                priority, orientation, best = 2, D, idx
+            elif priority > 3 and remaining[idx][(0 + D) % 2] < w and remaining[idx][(1 + D) % 2] == h:
+                priority, orientation, best = 3, D, idx
+            elif priority > 4 and remaining[idx][(0 + D) % 2] < w and remaining[idx][(1 + D) % 2] < h:
+                priority, orientation, best = 4, D, idx
+            elif priority > 5:
+                priority, orientation, best = 5, D, idx
+    if priority < 5:
+        if orientation == 0:
+            omega, d = remaining[best][0], remaining[best][1]
+        else:
+            omega, d = remaining[best][1], remaining[best][0]
+        result[best] = Rectangle(x, y, omega, d)
+        indices.remove(best)
+        if priority == 2:
+            recursive_packing(x, y + d, w, h - d, remaining, indices, result)
+        elif priority == 3:
+            recursive_packing(x + omega, y, w - omega, h)
+#        elif priority == 4:
