85b81688f527b689005cca0389ee7c688384b6a4
Maxim Bonnaerens <maxim@bonnaerens.be>
2016-12-09 12:32:48 +0100

    added docs to visualize

--- spp/visualize.py
+++ spp/visualize.py
@@ -4,6 +4,22 @@ from random import random
 
 
 def visualize(width, height, rectangles):
+    """
+    Visualization of the a strip of size width x height with the layout of the rectangles.
+    The rectangles are annotated with their place in the input list on the figure.
+
+    Parameters
+    ----------
+    width
+        size of the width of the strip
+
+    height
+        size of the height of the strip
+
+    rectangles : list of namedtuple('Rectangle', ['x', 'y', 'w', 'h'])
+        A list of rectangles. This contains bottom left x and y coordinate and
+        the width and height of every rectangle.
+    """
     fig = plt.figure()
     axes = fig.add_subplot(1, 1, 1)
     axes.add_patch(
