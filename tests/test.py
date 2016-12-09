fc8cc81681e23dde7d34ba7c5eb56c6e5b46d8e2
Maxim Bonnaerens <maxim@bonnaerens.be>
2016-12-09 12:46:33 +0100

    Changed logger from debug to info

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@ import logging
 
 # Setup logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 console_logger = logging.StreamHandler()
 console_logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
